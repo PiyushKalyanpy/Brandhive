0e708b85e62db729e40d011de806f27d0c6b4521
PiyushKalyanpy <kalyanpiyushp@gmail.com>
2023-03-08 21:15:03 +0530

    updated meta and graph protocols

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,15 +55,15 @@ export default function Home() {
       <NextSeo
         openGraph={{
           type: "website",
-          url: "https://www.example.com/page",
-          title: "Open Graph Title",
-          description: "Open Graph Description",
+          url: "https://brandhive.vercel.app/",
+          title: "Brandhive",
+          description: "Welcome to Brandhive,developed by Piyush Kalyan, the ultimate platform for generating new and creative brand names using the power of artificial intelligence (AI). Our website utilizes state-of-the-art algorithms and deep learning models to generate unique, catchy, and memorable brand names that will set your business apart from the competition.",
           images: [
             {
-              url: "./logo.svg",
+              url: "https://brandhive.vercel.app/ss.png",
               width: 800,
               height: 600,
-              alt: "Og Image Alt",
+              alt: "Brandhive",
             },
           ],
         }}
